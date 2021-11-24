f041a97e50cc788eef4d55b20591c0fad01b5c8a
Julen Crespo Jimenez <jcrespojimenez@danielcastelao.org>
2021-11-24 14:17:17 +0100

    Initial commit

---
+++ src/com/programacion/Singleton.java
@@ -0,0 +1,15 @@
+package com.programacion;
+
+public class Singleton {
+
+    private static Singleton _singelton;
+
+    private String nombre;
+
+    private Singleton() {
+    }
+
+    public static Singleton Instanciar(){
+
+    }
+}
